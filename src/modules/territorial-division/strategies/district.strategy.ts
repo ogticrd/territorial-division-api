da03aa283407cac99b0d8ffaff4b4ed9186dc987
Marluan Espiritusanto <marluanespiritusantoguerrero@gmail.com>
2021-07-29 13:49:46 -0400

    [ADD] district strategy

---
+++ src/modules/territorial-division/strategies/district.strategy.ts
@@ -0,0 +1,48 @@
+import { Raw, Repository } from 'typeorm';
+
+import { QueryDistrictDto } from '../dto';
+import { District } from '../entities';
+import { QueryStrategy } from '../interfaces';
+
+export class DistrictStrategy
+  implements QueryStrategy<District, QueryDistrictDto>
+{
+  find(
+    query: QueryDistrictDto,
+    repository: Repository<District>,
+  ): Promise<District | District[]> {
+    if (query.name) {
+      return repository.find({
+        where: {
+          name: Raw((name: string) => `LOWER(${name}) Like '%${query.name}%'`),
+        },
+      });
+    }
+
+    if (query.code) {
+      return repository.findOne({
+        where: { code: query.code },
+      });
+    }
+
+    if (query.municipalityCode) {
+      return repository.find({
+        where: { municipalityCode: query.municipalityCode },
+      });
+    }
+
+    if (query.provinceCode) {
+      return repository.find({
+        where: { provinceCode: query.provinceCode },
+      });
+    }
+
+    if (query.regionCode) {
+      return repository.find({
+        where: { regionCode: query.regionCode },
+      });
+    }
+
+    return repository.find();
+  }
+}
