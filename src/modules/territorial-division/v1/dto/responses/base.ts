3537319457676392b7e80eb272fb0fd946ee4da9
Marluan Espiritusanto <marluanespiritusantoguerrero@gmail.com>
2022-03-31 12:16:23 +0200

    fix: lint rules applied

--- src/modules/territorial-division/v1/dto/responses/base.ts
+++ src/modules/territorial-division/v1/dto/responses/base.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Exclude, Expose } from 'class-transformer';
+import { Expose } from 'class-transformer';
 import { IsString } from 'class-validator';
 
 export class ResponseBase {
--- src/modules/territorial-division/v1/entities/sub-neighborhood.model.ts
+++ src/modules/territorial-division/v1/entities/sub-neighborhood.model.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, ManyToOne } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 import { BaseModel } from './base.model';
 
 @Entity({ name: 'sub_neighborhoods' })
